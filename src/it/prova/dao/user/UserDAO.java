abb6c0e11d314ed0473dff63a7ce5aeaa444a659
Luciacal01 <luciacalabria80@gmail.com>
2022-05-18 15:44:55 +0200

    primo commit ProvaDAOSemplificataWhitService

---
+++ src/it/prova/dao/user/UserDAO.java
@@ -0,0 +1,17 @@
+package it.prova.dao.user;
+
+import java.util.Date;
+import java.util.List;
+
+import it.prova.dao.IBaseDAO;
+import it.prova.model.User;
+
+public interface UserDAO extends IBaseDAO<User> {
+	public List<User> findAllUsernameStartWhit(String iniziale) throws Exception;
+
+	public List<User> cercaTuttiQuelliCreatiPrimaDi(Date dataConfronto) throws Exception;
+
+	public List<User> cercaPerCognomeENomeCheInziaCon(String cognomeInput, String inzialeNomeInput) throws Exception;
+
+	public User accedi(String loginInput, String passwordInput) throws Exception;
+}
